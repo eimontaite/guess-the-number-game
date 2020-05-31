e5c056c4faf62ca0a130298d910e028a4a5e54ea
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-05-31 21:33:53 +0300

    Add NumberGenerator interface and the implementing class; configure and use a Spring container

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -2,11 +2,30 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
 
+	public static final String CONFIG_LOCATION = "beans.xml";
+
 	public static void main(String[] args) {
 		log.info("Guess The Number Game");
+
+		// create the context (container)
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+		NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+		// call next() to get a random number
+		int number = numberGenerator.next();
+
+		// log the number
+		log.info("number = {}", number);
+
+		// close the container
+		context.close();
+
 	}
 }
