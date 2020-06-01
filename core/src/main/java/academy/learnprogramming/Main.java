b379ac992f4987d4cf68679199830c8cc87ac6bd
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-06-01 14:01:57 +0300

    Use Java annotation configuration

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -3,18 +3,17 @@ package academy.learnprogramming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-	public static final String CONFIG_LOCATION = "beans.xml";
-
 	public static void main(String[] args) {
 		log.info("Guess The Number Game");
 
 		// create the context (container)
-		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+		ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
 		// get the number generator bean from the container
 		NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
