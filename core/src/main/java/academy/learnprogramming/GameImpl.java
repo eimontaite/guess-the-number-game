09bb68620d7608eabe0bb5c99aad58f46d01bf12
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-06-01 13:48:36 +0300

    Remove unused code in GameImpl

--- core/src/main/java/academy/learnprogramming/GameImpl.java
+++ core/src/main/java/academy/learnprogramming/GameImpl.java
@@ -25,11 +25,6 @@ public class GameImpl implements Game {
 	private int remainingGuesses;
 	private boolean validNumberRange = true;
 
-//	// == constructors ==
-//	public GameImpl(NumberGenerator numberGenerator) {
-//		this.numberGenerator = numberGenerator;
-//	}
-
 	// == init ==
 	@PostConstruct
 	@Override
