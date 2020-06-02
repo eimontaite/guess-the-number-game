d5de178380b5bbb70b41cc9bd8dec05b2f203258
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-06-02 05:54:01 +0300

    Optimize code using Lombok

--- console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
+++ console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
@@ -2,18 +2,16 @@ package academy.learnprogramming.console;
 
 import academy.learnprogramming.Game;
 import academy.learnprogramming.MessageGenerator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 
+@Slf4j
 @Component
 public class ConsoleNumberGuess {
-	// == constants ==
-	public static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
 	// == fields ==
 	private final Game game;
