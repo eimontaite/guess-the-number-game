47f2522d2a1ba7cd022e198989f29fa3d4383980
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-06-08 19:14:04 +0300

    Add a play template and related controller

---
+++ web/src/main/java/academy/learnprogramming/util/GameMappings.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.util;
+
+public final class GameMappings {
+	// == constants ==
+	public static final String PLAY = "play";
+	public static final String REDIRECT = "redirect:/" + PLAY;
+
+	// == constructor ==
+	private GameMappings() {
+	}
+}
