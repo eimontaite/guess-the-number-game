b9c92d9297a3a8d3dda262c0bba12cf749b72b68
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-06-08 18:20:10 +0300

    Set up Thymeleaf and add initial template

---
+++ web/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming.config;
+
+import academy.learnprogramming.util.ViewNames;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		log.info("Adding view controller");
+		registry.addViewController("/").setViewName(ViewNames.HOME);
+	}
+}
